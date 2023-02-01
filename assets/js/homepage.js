98f8d58805a61eb0219af764a3372716a205ef66
IlBennys <119436512+IlBennys@users.noreply.github.com>
2023-02-01 19:38:36 +0100

    barra

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,23 +1,23 @@
-let artist = "eminem";
-let artist2 = "dreamtheater";
+let artist = "eminem"
+let artist2 = "dreamtheater"
 const playList = async function (song) {
   let arrSongs = await fetch(
     `https://striveschool-api.herokuapp.com/api/deezer/search?q=${song}`
-  );
-  let songs = await arrSongs.json();
+  )
+  let songs = await arrSongs.json()
 
-  return songs;
-};
+  return songs
+}
 const selectedAlbum = async function (song) {
   let linkFetch = await fetch(
     `https://striveschool-api.herokuapp.com/api/deezer/search?q=${song}`
-  );
-  let album = await linkFetch.json();
-  return album;
-};
+  )
+  let album = await linkFetch.json()
+  return album
+}
 function cycleArr(array) {
-  console.log(array);
-  let rowContainer = document.querySelector("#liked-songs");
+  console.log(array)
+  let rowContainer = document.querySelector("#liked-songs")
   for (let i = 0; i < 6; i++) {
     rowContainer.innerHTML += `<div class="your-albums p-0" id="list-${i}" onclick="songId(${array[i].album.id})">
             <div class="albums-img">
@@ -26,14 +26,14 @@ function cycleArr(array) {
             <div class="albums-text">
                 <p>${array[i].title}</p>
             </div>
-        </div>`;
+        </div>`
   }
-  let cardsContainer = document.querySelector("#title1");
-  let titleAlbum = document.createElement("p");
-  titleAlbum.innerText = `${array[0].artist.name}`;
-  titleAlbum.classList.add('mt-4')
-  cardsContainer.appendChild(titleAlbum);
-  let cardsAlbum = document.querySelector("#album-cards1");
+  let cardsContainer = document.querySelector("#title1")
+  let titleAlbum = document.createElement("p")
+  titleAlbum.innerText = `${array[0].artist.name}`
+  titleAlbum.classList.add("mt-4")
+  cardsContainer.appendChild(titleAlbum)
+  let cardsAlbum = document.querySelector("#album-cards1")
   for (let i = 0; i < 5; i++) {
     cardsAlbum.innerHTML += `<div class="card album-cards" onclick="songId(${array[i].album.id})">
             <img src="${array[i].album.cover_medium}" class="card-img-top" alt="...">
@@ -41,12 +41,12 @@ function cycleArr(array) {
             <h5 class="card-title fs-6 ">${array[i].album.title}</h5>
             <a href="./artist.html?id=${array[i].artist.id}"class="card-text">${array[i].artist.name}</a>
         </div>
-    </div>`;
+    </div>`
   }
 }
 function secondAlbumCycle(array) {
-  console.log(array);
-  let singleAlb = document.querySelector("#single-album");
+  console.log(array)
+  let singleAlb = document.querySelector("#single-album")
   singleAlb.innerHTML = `<div class="p-0" id="announced-img">
             <img src="${array[18].album.cover_medium}"  alt="">
         </div>
@@ -59,13 +59,13 @@ function secondAlbumCycle(array) {
             <button id="single-btn2">Salva</button>
         </div>
     </div>
-        `;
-  let cardsContainer = document.querySelector("#title2");
-  let titleAlbum = document.createElement("p");
-  titleAlbum.innerText = `${array[0].artist.name}`;
-  titleAlbum.classList.add('mt-4')
-  cardsContainer.appendChild(titleAlbum);
-  let cardsAlbum = document.querySelector("#album-cards2");
+        `
+  let cardsContainer = document.querySelector("#title2")
+  let titleAlbum = document.createElement("p")
+  titleAlbum.innerText = `${array[0].artist.name}`
+  titleAlbum.classList.add("mt-4")
+  cardsContainer.appendChild(titleAlbum)
+  let cardsAlbum = document.querySelector("#album-cards2")
 
   for (let i = 0; i < 5; i++) {
     cardsAlbum.innerHTML += `
@@ -75,8 +75,7 @@ function secondAlbumCycle(array) {
             <h5 class="card-title fs-6 ">${array[i].album.title}</h5>
             <a href="./artist.html?id=${array[i].artist.id}"class="card-text">${array[i].artist.name}</a>
         </div>
-    </div>`;
-   
+    </div>`
   }
 }
 function songId(id) {
@@ -84,7 +83,7 @@ function songId(id) {
 }
 playList(artist).then(async function (data) {
   await selectedAlbum(artist2).then(function (data) {
-    secondAlbumCycle(data.data);
-  });
-  cycleArr(data.data);
-});
+    secondAlbumCycle(data.data)
+  })
+  cycleArr(data.data)
+})
