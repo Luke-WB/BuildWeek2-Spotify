62fe5f03c41aaeda77d4c0277fb516070606dd6b
Luca Scomazzon <scom.luca@gmail.com>
2023-02-02 20:18:43 +0100

    Fix totale

--- assets/js/album-page.js
+++ assets/js/album-page.js
@@ -1,7 +1,8 @@
-const Url = "https://striveschool-api.herokuapp.com/api/deezer/album/"
+const Url = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+const UrlSong = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
 
 const fetchQuery = async (query) => {
-  const album = await fetch(`${Url}${query}`)
+  const album = await fetch(`${Url}${query}`);
   const {
     id,
     title,
@@ -10,8 +11,8 @@ const fetchQuery = async (query) => {
     artist,
     release_date,
     tracks: { data },
-  } = await album.json()
-  const divSopra = document.querySelector(".sopra")
+  } = await album.json();
+  const divSopra = document.querySelector(".sopra");
   divSopra.innerHTML += `
   <div class="foto-album">
     <img src="${cover_xl}" alt="Foto Album" style="width: 200px; height: 200px;" />
@@ -40,18 +41,19 @@ const fetchQuery = async (query) => {
       </div>
     </div>
   </div>
-            `
-  return data
-}
+            `;
+  return data;
+};
 
 const album = async (id) => {
-  const canzoni = await fetchQuery(id)
-  console.log(canzoni)
-  const divSotto = document.querySelector(".elenco")
+  const canzoni = await fetchQuery(id);
+  console.log(canzoni);
+  const divSotto = document.querySelector(".elenco");
   for (let i = 0; i < canzoni.length; i++) {
-    const singoli = canzoni[i]
+    const singoli = canzoni[i];
     divSotto.innerHTML += `
-    <div class="canzoni row p-0 align-items-center m-0 mb-3">
+    <div class="canzoni row p-0 align-items-center m-0 mb-3" 
+    onclick="canzoniSingole('${singoli.title}','${singoli.artist.name}')">
       <div class="col-1 p-0 text-center">${i + 1}</div>
       <div class="col-4 p-0 text-start">
         <span class="fw-bold text-white">${singoli.title}</span>
@@ -60,41 +62,58 @@ const album = async (id) => {
       </div>
       <div class="col-3 p-0 text-end" >${singoli.rank}</div>
       <div class="col-3 p-0 text-end">
-      ${Math.floor(singoli.duration / 60)}:${
-      singoli.duration - Math.floor(singoli.duration / 60) * 60
-    }
+      ${Math.floor(singoli.duration / 60)}:${singoli.duration - Math.floor(singoli.duration / 60) * 60}
     </div>
-    `
+    `;
   }
-}
+};
+
+const canzoniSingole = async (title, name) => {
+  console.log(title);
+  const song = await fetch(`${UrlSong}${title}${name}`);
+  const { data } = await song.json();
+  console.log(data);
+  const primaCanzone = data[0];
+  const canzoneSingola = document.querySelector(".singoli");
+  canzoneSingola.innerHTML = `
+  <div class="col-6">
+    <img src="${primaCanzone.album.cover_xl}" alt="Foto album" style="width: 60px; height: 60px"/>
+  </div>
+  <div class="col-6 d-flex align-items-center">
+    <span class="fw-bold">${primaCanzone.title} </span>
+    <br>
+    ${primaCanzone.artist.name}
+  </div>
+  `;
+};
 
 window.onload = async () => {
-  let url = new URLSearchParams(location.search)
-  let id = url.get("id")
-  console.log(id)
+  let url = new URLSearchParams(location.search);
+  let id = url.get("id");
+  console.log(id);
   if (!id) {
-    window.location.assign("./homepage.html")
+    window.location.assign("./homepage.html");
   }
-  await album(id)
-}
+  await album(id);
+};
 
 window.onscroll = function () {
-  const myNav = document.querySelector(".navSopra")
+  const myNav = document.querySelector(".navSopra");
 
   if (window.scrollY > 80) {
-    myNav.classList.add("nav-colored")
+    myNav.classList.add("nav-colored");
     // myNav.classList.remove("nav-transparent")
   } else {
     // myNav.classList.add("nav-transparent")
-    myNav.classList.remove("nav-colored")
+    myNav.classList.remove("nav-colored");
   }
-}
+};
 
 const bottiniCambiati = () => {
-  const prev = document.querySelector("#bottonesUno")
-  window.history.back(prev)
-}
+  const prev = document.querySelector("#bottonesUno");
+  window.history.back(prev);
+};
 const bottiniCambiatiDue = () => {
-  const next = document.querySelector("#bottonesDue")
-  window.history.forward(next)
-}
+  const next = document.querySelector("#bottonesDue");
+  window.history.forward(next);
+};
--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,19 +1,20 @@
-const APIUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/"
+const APIUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
+const UrlSong = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
 
 const fetchByQuery = async (idi) => {
-  console.log(idi)
-  const res = await fetch(`${APIUrl}${idi}`)
+  console.log(idi);
+  const res = await fetch(`${APIUrl}${idi}`);
   // console.log(res)
-  const { tracklist, picture_xl, nb_fan, name } = await res.json()
-  const tracce = await fetch(tracklist)
-  const { data } = await tracce.json()
+  const { tracklist, picture_xl, nb_fan, name } = await res.json();
+  const tracce = await fetch(tracklist);
+  const { data } = await tracce.json();
   // console.log(tracklist)
-  const nomeArtista = name
-  console.log(nomeArtista)
+  const nomeArtista = name;
+  console.log(nomeArtista);
 
   // console.log()
 
-  let divImg = document.querySelector("#immagine")
+  let divImg = document.querySelector("#immagine");
   divImg.innerHTML += `
   
   
@@ -36,8 +37,8 @@ const fetchByQuery = async (idi) => {
   top: 42%;
   color: white; margin: 0.08em 3px 0.12em;">${nb_fan} ascoltatori mensili</p>
   
-  `
-  const imgArtista = document.querySelector("#artista-img")
+  `;
+  const imgArtista = document.querySelector("#artista-img");
   imgArtista.innerHTML += ` <img
   src="${picture_xl}"
   class="rounded-circle position-static"
@@ -49,63 +50,77 @@ const fetchByQuery = async (idi) => {
   </div>
   <p class="par m-0 d-flex flex-column justify-content-center">Hai messo Mi piace a 11 brani <br><span style="color:#a2adb3;" class="par m-0">Di ${nomeArtista}</span>
   </p>
-  `
+  `;
 
-  return data
-}
+  return data;
+};
 
 const renderFavoriteSongs = async (id) => {
-  let canzoni = await fetchByQuery(id)
+  let canzoni = await fetchByQuery(id);
   // console.log(canzoni)
   for (let i = 0; i < canzoni.length; i++) {
-    const canzonePrincipale = canzoni[i]
-    const div = document.querySelector("#carta")
+    const canzonePrincipale = canzoni[i];
+    const div = document.querySelector("#carta");
     // console.log(div)
     div.innerHTML += `
-    <div class="row p-0 m-0 mb-3 text-light align-items-center cambio">
+    <div class="row p-0 m-0 mb-3 text-light align-items-center cambio"
+    onclick="canzoniSingole('${canzonePrincipale.title}','${canzonePrincipale.artist.name}')">
     <div class="col-1 text-light text-center ">${i + 1}</div>
-    <div class="col-2  "><img src="${
-      canzonePrincipale.album.cover_xl
-    }" alt="" style="width: 60px;"></div>
-    <div class="col-5  text-light text-start ">${
-      canzonePrincipale.title_short
-    }</div>
+    <div class="col-2  "><img src="${canzonePrincipale.album.cover_xl}" alt="" style="width: 60px;"></div>
+    <div class="col-5  text-light text-start ">${canzonePrincipale.title_short}</div>
     <div class="col-2  text-light">${canzonePrincipale.rank}</div>
     ${Math.floor(canzonePrincipale.duration / 60)}:${
-      canzonePrincipale.duration -
-      [Math.floor(canzonePrincipale.duration / 60) * 60]
+      canzonePrincipale.duration - [Math.floor(canzonePrincipale.duration / 60) * 60]
     }</div>
     </div>
-    `
+    `;
   }
-}
+};
+
+const canzoniSingole = async (title, name) => {
+  const song = await fetch(`${UrlSong}${title}${name}`);
+  const { data } = await song.json();
+  console.log(data);
+  const primaCanzone = data[0];
+  const canzoneSingola = document.querySelector(".singoli");
+  canzoneSingola.innerHTML = `
+  <div class="col-6">
+    <img src="${primaCanzone.album.cover_xl}" alt="Foto album" style="width: 60px; height: 60px"/>
+  </div>
+  <div class="col-6 d-flex align-items-center">
+    <span class="fw-bold">${primaCanzone.title} </span>
+    <br>
+    ${primaCanzone.artist.name}
+  </div>
+  `;
+};
 
 window.onload = async () => {
-  let url = new URLSearchParams(location.search)
-  let id = url.get("id")
+  let url = new URLSearchParams(location.search);
+  let id = url.get("id");
   if (!id) {
-    window.location.assign("./homepage.html")
+    window.location.assign("./homepage.html");
   }
-  await renderFavoriteSongs(id)
-}
+  await renderFavoriteSongs(id);
+};
 
 window.onscroll = function () {
-  const myNav = document.querySelector(".navSopra")
+  const myNav = document.querySelector(".navSopra");
 
   if (window.scrollY > 80) {
-    myNav.classList.add("nav-colored")
+    myNav.classList.add("nav-colored");
     // myNav.classList.remove("nav-transparent")
   } else {
     // myNav.classList.add("nav-transparent")
-    myNav.classList.remove("nav-colored")
+    myNav.classList.remove("nav-colored");
   }
-}
+};
 
 const bottiniCambiati = () => {
-  const prev = document.querySelector("#bottonesUno")
-  window.history.back(prev)
-}
+  const prev = document.querySelector("#bottonesUno");
+  window.history.back(prev);
+};
 const bottiniCambiatiDue = () => {
-  const next = document.querySelector("#bottonesDue")
-  window.history.forward(next)
-}
+  const next = document.querySelector("#bottonesDue");
+  window.history.forward(next);
+};
