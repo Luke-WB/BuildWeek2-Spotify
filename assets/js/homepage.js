032d6a86671e98b6e9c4f7469775ce41c9671151
Luca Scomazzon <scom.luca@gmail.com>
2023-02-03 13:13:39 +0100

    Search-page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,23 +1,19 @@
-let artist = "eminem"
-let artist2 = "dreamtheater"
+let artist = "eminem";
+let artist2 = "dreamtheater";
 const playList = async function (song) {
-  let arrSongs = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/search?q=${song}`
-  )
-  let songs = await arrSongs.json()
+  let arrSongs = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${song}`);
+  let songs = await arrSongs.json();
 
-  return songs
-}
+  return songs;
+};
 const selectedAlbum = async function (song) {
-  let linkFetch = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/search?q=${song}`
-  )
-  let album = await linkFetch.json()
-  return album
-}
+  let linkFetch = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${song}`);
+  let album = await linkFetch.json();
+  return album;
+};
 function cycleArr(array) {
-  console.log(array)
-  let rowContainer = document.querySelector("#liked-songs")
+  console.log(array);
+  let rowContainer = document.querySelector("#liked-songs");
   for (let i = 0; i < 6; i++) {
     rowContainer.innerHTML += `<div class="your-albums p-0" id="list-${i}" onclick="songId(${array[i].album.id})">
             <div class="albums-img">
@@ -26,14 +22,14 @@ function cycleArr(array) {
             <div class="albums-text">
                 <p>${array[i].title}</p>
             </div>
-        </div>`
+        </div>`;
   }
-  let cardsContainer = document.querySelector("#title1")
-  let titleAlbum = document.createElement("p")
-  titleAlbum.innerText = `${array[0].artist.name}`
-  titleAlbum.classList.add("mt-4")
-  cardsContainer.appendChild(titleAlbum)
-  let cardsAlbum = document.querySelector("#album-cards1")
+  let cardsContainer = document.querySelector("#title1");
+  let titleAlbum = document.createElement("p");
+  titleAlbum.innerText = `${array[0].artist.name}`;
+  titleAlbum.classList.add("mt-4");
+  cardsContainer.appendChild(titleAlbum);
+  let cardsAlbum = document.querySelector("#album-cards1");
   for (let i = 0; i < 5; i++) {
     cardsAlbum.innerHTML += `<div class="card album-cards" onclick="songId(${array[i].album.id})">
             <img src="${array[i].album.cover_medium}" class="card-img-top" alt="...">
@@ -41,12 +37,12 @@ function cycleArr(array) {
             <h5 class="card-title fs-6 ">${array[i].album.title}</h5>
             <a href="./artist.html?id=${array[i].artist.id}"class="card-text">${array[i].artist.name}</a>
         </div>
-    </div>`
+    </div>`;
   }
 }
 function secondAlbumCycle(array) {
-  console.log(array)
-  let singleAlb = document.querySelector("#single-album")
+  console.log(array);
+  let singleAlb = document.querySelector("#single-album");
   singleAlb.innerHTML = `<div class="p-0" id="announced-img">
             <img src="${array[18].album.cover_medium}"  alt="">
         </div>
@@ -59,52 +55,52 @@ function secondAlbumCycle(array) {
             <button id="single-btn2">Salva</button>
         </div>
     </div>
-        `
-  let cardsContainer = document.querySelector("#title2")
-  let titleAlbum = document.createElement("p")
-  titleAlbum.innerText = `${array[0].artist.name}`
-  titleAlbum.classList.add("mt-4")
-  cardsContainer.appendChild(titleAlbum)
-  let cardsAlbum = document.querySelector("#album-cards2")
+        `;
+  let cardsContainer = document.querySelector("#title2");
+  let titleAlbum = document.createElement("p");
+  titleAlbum.innerText = `${array[0].artist.name}`;
+  titleAlbum.classList.add("mt-4");
+  cardsContainer.appendChild(titleAlbum);
+  let cardsAlbum = document.querySelector("#album-cards2");
 
   for (let i = 0; i < 5; i++) {
     cardsAlbum.innerHTML += `
     <div class="card album-cards"  onclick="songId(${array[i].album.id})">
-            <img src="${array[i].album.cover_medium}" class="card-img-top" alt="...">
+            <img src="${array[i].album.cover_medium}" class="card-img-top" alt="Foto album">
         <div class="card-body d-flex flex-column justify-content-between py-3 px-0">
             <h5 class="card-title fs-6 ">${array[i].album.title}</h5>
             <a href="./artist.html?id=${array[i].artist.id}"class="card-text">${array[i].artist.name}</a>
         </div>
-    </div>`
+    </div>`;
   }
 }
 function songId(id) {
-  id.onclick = location.assign(`./album-page.html?id=${id}`)
+  id.onclick = location.assign(`./album-page.html?id=${id}`);
 }
 selectedAlbum(artist2).then(function (data) {
- secondAlbumCycle(data.data)
-})
+  secondAlbumCycle(data.data);
+});
 playList(artist).then(async function (data) {
-  cycleArr(data.data)
-})
+  cycleArr(data.data);
+});
 
 window.onscroll = function () {
-  const myNav = document.querySelector(".navSopra")
+  const myNav = document.querySelector(".navSopra");
 
   if (window.scrollY > 80) {
-    myNav.classList.add("nav-colored")
+    myNav.classList.add("nav-colored");
     // myNav.classList.remove("nav-transparent")
   } else {
     // myNav.classList.add("nav-transparent")
-    myNav.classList.remove("nav-colored")
+    myNav.classList.remove("nav-colored");
   }
-}
+};
 
 const bottiniCambiati = () => {
-  const prev = document.querySelector("#bottonesUno")
-  window.history.back(prev)
-}
+  const prev = document.querySelector("#bottonesUno");
+  window.history.back(prev);
+};
 const bottiniCambiatiDue = () => {
-  const next = document.querySelector("#bottonesDue")
-  window.history.forward(next)
-}
+  const next = document.querySelector("#bottonesDue");
+  window.history.forward(next);
+};
--- assets/js/search.js
+++ assets/js/search.js
@@ -2,6 +2,35 @@ const Url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
 
 const ricerca = async (query) => {
   const song = await fetch(`${Url}${query}`);
-  const songCercate = await sessionStorage.json();
-  console.log(songCercate);
+  const { data } = await song.json();
+  return data;
+};
+
+const music = async (name) => {
+  const searchMusic = await ricerca(name);
+  /* console.log(searchMusic); */
+  if (searchMusic[0].artist.name == name) {
+    const divArtista = document.querySelector(".artista");
+    divArtista.innerHTML += `
+      <div class="text-center">
+        <img src="${searchMusic[0].artist.picture_xl}" alt="Foto artista" class="foto" />
+        <h6 class="mt-3 fw-bold">${searchMusic[0].artist.name}</h6>
+      </div>
+      `;
+
+    searchMusic.forEach((singolaSearch) => {
+      const divArtista = document.querySelector(".canzoni");
+      divArtista.innerHTML += `
+        <div class="card album-cards col-2 justify-content-center">
+          <img src="${singolaSearch.album.cover_xl}" class="foto" alt="Foto album">
+          <div class="card-body d-flex flex-column justify-content-between py-3 px-0">
+            <h5 class="card-title fs-6 ">${singolaSearch.album.title}</h5>
+            <a href="./artist.html?id=${singolaSearch.artist.id}"class="card-text">${singolaSearch.artist.name}</a>
+          </div>
+        </div>`;
+    });
+  }
+};
+window.onload = async () => {
+  await music("Avicii");
 };
